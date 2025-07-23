c7b31d6460066e0c7068181bd640ef0056f00699
nidheeshentri <nidheesh.b@entri.me>
2025-07-23 21:08:54 +0530

    header import fixed

--- src/layout/UserLayout.jsx
+++ src/layout/UserLayout.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
-import Header from '../components/HEader'
+import Header from '../components/Header'
 
 const UserLayout = () => {
   return (
